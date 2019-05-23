a6a1ef37e07bae22a7b4eac5b7c660b53aacf91a
Kazutoshi Shirai <kazsh@kazsh.com>
2019-05-23 12:19:57 +0900

    [update] cacheを増やした

--- docs/assets/js/service_worker.js
+++ docs/assets/js/service_worker.js
@@ -1,7 +1,12 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'instag-caches';
 var urlsToCache = [
-    '/kazsh2016.github.io/instag/',
+    '/kazsh2016.github.io/instag/index.html',
+    '/kazsh2016.github.io/instag/assets/js/index.js',
+    '/kazsh2016.github.io/instag/assets/js/service_worker.js',
+    '/kazsh2016.github.io/instag/assets/js/data.json',
+    '/kazsh2016.github.io/instag/assets/css/reset.css',
+    '/kazsh2016.github.io/instag/assets/css/style.css',
 ];
 
 // インストール処理
